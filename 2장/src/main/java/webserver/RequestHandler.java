4dae99640e0bcb77d0caa665aa038ba26b4c5af7
Jongmin Yoon <jongmin4943@gmail.com>
2022-01-12 00:21:27 +0900

    2장 요구사항 2 - GET 방식으로 회원가입하기 완료

--- 2장/src/main/java/webserver/RequestHandler.java
+++ 2장/src/main/java/webserver/RequestHandler.java
@@ -4,9 +4,13 @@ import java.io.*;
 import java.net.Socket;
 import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
+import java.util.Arrays;
+import java.util.Map;
 
+import model.User;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import util.HttpRequestUtils;
 
 public class RequestHandler extends Thread {
     private static final Logger log = LoggerFactory.getLogger(RequestHandler.class);
@@ -31,10 +35,21 @@ public class RequestHandler extends Thread {
             while(!"".equals(line = br.readLine())) {
                 if(line == null) return; // null 이 들어올경우 반복문을 종료한다.
                 System.out.println(line);
-                String requestUrl = extractUrl(line);
-                // requestUrl 이 있다면 webapp 폴더에 있는 요청된 파일을 읽어서 byte 로 변환해 body 에 넣어준다.
-                if(hasText(requestUrl)) continue;
-                body = Files.readAllBytes(new File("./webapp" + requestUrl).toPath());
+                String requestUrl = extractRequestUrl(line);
+                // requestUrl 이 있다면 webapp 폴더에 있는 요청된 파일을 읽어서 byte 로 변환해 body 에 넣어주고 반복문을 종료한다.
+                if(hasText(requestUrl)) {
+                    String url = extractUrl(requestUrl);
+                    String params = extractParams(requestUrl); // params 가 있으면 ? 뒤의 String 이 추출된다.
+
+                    // url 이 뭔지 판단하는 함수.
+                    if(checkPathVariables(url,"user","create")) {
+                        // user/create 일경우 유저를 넘어온 params 값으로 생성한다.
+                        User user = createUserByParams(params);
+                        System.out.println(user);
+                    }
+                    body = Files.readAllBytes(new File("./webapp" + url).toPath());
+                    break;
+                }
             }
             response200Header(dos, body.length);
             responseBody(dos, body);
@@ -43,7 +58,34 @@ public class RequestHandler extends Thread {
         }
     }
 
-    private String extractUrl(String line) {
+    private boolean checkPathVariables(String url, String ... paths) {
+        // url 은 시작이 / 이므로 없애준다.
+        url = url.substring(1);
+
+        // ex) user/create -> [user, create]
+        String[] pathVariables = url.split("/");
+        boolean result = true;
+
+        // 넘어온 체크할 paths 를 비교한다.
+        for (int i = 0; i < paths.length; i++) {
+            if(!paths[i].equals(pathVariables[i])) {
+                return false;
+            }
+        }
+        return result;
+    }
+
+    private User createUserByParams(String params) {
+        // 넘어온 params 를 추출해 유저 생성
+        Map<String, String> paramsMap = HttpRequestUtils.parseQueryString(params);
+        String userId = paramsMap.get("userId");
+        String password = paramsMap.get("password");
+        String name = paramsMap.get("name");
+        String email = paramsMap.get("email");
+        return new User(userId,password,name,email);
+    }
+
+    private String extractRequestUrl(String line) {
         // 줄마다 띄어쓰기로 정보가 나눠져 있으므로 split 을 한다.
         String[] token = line.split(" ");
         String requestUrl = "";
@@ -54,8 +96,19 @@ public class RequestHandler extends Thread {
         return requestUrl;
     }
 
+    private String extractUrl(String requestUrl) {
+        int index = requestUrl.indexOf("?");
+        return requestUrl.substring(0,index);
+    }
+
+    private String extractParams(String requestUrl) {
+        int index = requestUrl.indexOf("?");
+        if(index < 0) return "";
+        return requestUrl.substring(index+1);
+    }
+
     private boolean hasText(String str) {
-        return str == null || str.isEmpty();
+        return !(str == null || str.isEmpty());
     }
 
     private void response200Header(DataOutputStream dos, int lengthOfBodyContent) {
