591cd33221712c730d72438fa7c2821c92a4f385
Jongmin Yoon <jongmin4943@gmail.com>
2022-01-13 23:30:48 +0900

    2장 요구사항 3 - POST 방식으로 회원가입하기 완료" ;

--- 2장/src/main/java/webserver/RequestHandler.java
+++ 2장/src/main/java/webserver/RequestHandler.java
@@ -5,6 +5,7 @@ import java.net.Socket;
 import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.util.Arrays;
+import java.util.List;
 import java.util.Map;
 
 import model.User;
@@ -15,7 +16,8 @@ import util.HttpRequestUtils;
 public class RequestHandler extends Thread {
     private static final Logger log = LoggerFactory.getLogger(RequestHandler.class);
 
-    private Socket connection;
+    private final Socket connection;
+    private final List<String> requestMethods = Arrays.asList("GET","POST","PUT","DELETE");
 
     public RequestHandler(Socket connectionSocket) {
         this.connection = connectionSocket;
@@ -30,21 +32,35 @@ public class RequestHandler extends Thread {
             // InputStream 을 Character input stream 으로 변환하기위해 BufferedReader 를 사용한다.
             // InputStreamReader 를 이용해 InputStream 을 읽어서 Reader 객체를 만든다.
             BufferedReader br = new BufferedReader(new InputStreamReader(in, StandardCharsets.UTF_8));
-            String line;
             // 헤더의 마지막은 공백이므로 공백이 아닐때까지 읽어들인다.
-            while(!"".equals(line = br.readLine())) {
-                if(line == null) return; // null 이 들어올경우 반복문을 종료한다.
+            while(true) {
+                String line = br.readLine();
+                if(line == null) break; // null 이 들어올경우 반복문을 종료한다.
                 System.out.println(line);
-                String requestUrl = extractRequestUrl(line);
+                String[] requestMethodAndUrl = extractRequestMethodAndUrl(line);
+                String requestMethod = requestMethodAndUrl[0];
+                String requestUrl = requestMethodAndUrl[1];
                 // requestUrl 이 있다면 webapp 폴더에 있는 요청된 파일을 읽어서 byte 로 변환해 body 에 넣어주고 반복문을 종료한다.
                 if(hasText(requestUrl)) {
                     String url = extractUrl(requestUrl);
                     String params = extractParams(requestUrl); // params 가 있으면 ? 뒤의 String 이 추출된다.
+                    int contentLength = 0;
 
+                    // 헤더에서 Content-Length 추출
+                    while(!"".equals(line = br.readLine())) {
+                        if(line == null) break;
+                        if(line.startsWith("Content-Length")) {
+                            String[] temp = line.split(" ");
+                            contentLength = Integer.parseInt(temp[1]);
+                            break;
+                        }
+                    }
                     // url 이 뭔지 판단하는 함수.
                     if(checkPathVariables(url,"user","create")) {
+                        while(!"".equals(br.readLine())) {} // header 종료 후 body 읽기
+                        String requestBody = util.IOUtils.readData(br,contentLength);
                         // user/create 일경우 유저를 넘어온 params 값으로 생성한다.
-                        User user = createUserByParams(params);
+                        User user = createUserByParams(requestBody);
                         System.out.println(user);
                     }
                     body = Files.readAllBytes(new File("./webapp" + url).toPath());
@@ -85,20 +101,20 @@ public class RequestHandler extends Thread {
         return new User(userId,password,name,email);
     }
 
-    private String extractRequestUrl(String line) {
+    private String[] extractRequestMethodAndUrl(String line) {
         // 줄마다 띄어쓰기로 정보가 나눠져 있으므로 split 을 한다.
         String[] token = line.split(" ");
-        String requestUrl = "";
-        // 첫번째 값이 GET 이라는 글자일 경우 두번째 값은 요청 url 이 된다.
-        if("GET".equals(token[0])) {
-            requestUrl = token[1];
+        // 첫번째 값이 requestMethods 값중 하나일 경우 두번째 값은 요청 url 이 된다.
+        if(requestMethods.contains(token[0])) {
+            return token;
         }
-        return requestUrl;
+        return new String[]{"",""};
     }
 
     private String extractUrl(String requestUrl) {
         int index = requestUrl.indexOf("?");
-        return requestUrl.substring(0,index);
+        if("/".equals(requestUrl)) requestUrl = "/index.html";
+        return index == -1 ? requestUrl : requestUrl.substring(0,index);
     }
 
     private String extractParams(String requestUrl) {
