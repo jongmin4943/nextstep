005c30b21ddd05632dad84b225899a045ad23c55
Jongmin Yoon <jongmin4943@gmail.com>
2022-02-09 00:53:58 +0900

    jdbc 리팩토링 재학습

--- 3장/src/main/java/core/jdbc/PreparedStatementSetter.java
+++ 3장/src/main/java/core/jdbc/PreparedStatementSetter.java
@@ -4,6 +4,7 @@ package core.jdbc;
 import java.sql.PreparedStatement;
 import java.sql.SQLException;
 
+@FunctionalInterface
 public interface PreparedStatementSetter {
     void setParameters(PreparedStatement pstmt) throws SQLException;
 }
