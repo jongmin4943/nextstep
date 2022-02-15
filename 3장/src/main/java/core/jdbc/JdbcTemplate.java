74d7e7def0376f5419a428d5de026596122072d0
Jongmin Yoon <jongmin4943@gmail.com>
2022-02-15 22:51:30 +0900

    jdbc 리팩토링 인강을  통한 재 학습

--- 3장/src/main/java/core/jdbc/JdbcTemplate.java
+++ 3장/src/main/java/core/jdbc/JdbcTemplate.java
@@ -18,6 +18,11 @@ public class JdbcTemplate {
         }
     }
 
+    public void executeUpdate(String sql, Object... parameters) {
+        PreparedStatementSetter pss = createPreparedStatementSetter(parameters);
+        this.update(sql,pss);
+    }
+
     public <T> List<T> query(String sql, PreparedStatementSetter pss, RowMapper<T> rm) throws DataAccessException {
         ResultSet rs = null;
         try (Connection con = ConnectionManager.getConnection();
@@ -26,8 +31,7 @@ public class JdbcTemplate {
             rs = pstmt.executeQuery();
             List<T> result = new ArrayList<>();
             if (rs.next()) {
-                T obj = rm.mapRow(rs);
-                result.add(obj);
+                result.add(rm.mapRow(rs));
             }
             return result;
         } catch (SQLException e) {
@@ -36,16 +40,28 @@ public class JdbcTemplate {
     }
 
     public <T> T queryForObject(String sql, PreparedStatementSetter pss, RowMapper<T> rm) throws DataAccessException {
-        ResultSet rs = null;
-        try (Connection con = ConnectionManager.getConnection();
-             PreparedStatement pstmt = con.prepareStatement(sql)){
-            pss.values(pstmt);
-
-            rs = pstmt.executeQuery();
-
-            return rm.mapRow(rs);
-        } catch (SQLException e) {
-            throw new DataAccessException(e);
+        List<T> result = query(sql,pss,rm);
+        if(result.isEmpty()) {
+            return null;
         }
+        return result.get(0);
+    }
+
+
+    public <T> T executeQuery(String sql, RowMapper<T> rm,  Object... parameters) throws DataAccessException {
+        return this.queryForObject(sql,createPreparedStatementSetter(parameters),rm);
+    }
+
+    public <T> List<T> executeQueryForList(String sql, RowMapper<T> rm,  Object... parameters) throws DataAccessException {
+        return this.query(sql,createPreparedStatementSetter(parameters),rm);
+    }
+
+    private PreparedStatementSetter createPreparedStatementSetter(Object[] parameters) {
+        return pstmt -> {
+            for (int i = 0; i < parameters.length; i++) {
+                pstmt.setObject(i+1, parameters[i]);
+            }
+            return pstmt;
+        };
     }
 }
--- 3장/src/main/java/next/dao/UserDao.java
+++ 3장/src/main/java/next/dao/UserDao.java
@@ -5,55 +5,40 @@ import next.model.User;
 
 import java.util.List;
 
+/**
+ * 동영상을 통한 재 학습 -> https://www.youtube.com/watch?v=ylrMBeakVnk
+ * 1. 개발자가 구현해야하는부분과 라이브러리로 구현하는 부분을 나눈다.
+ * 2. 리팩토링 도중 항상 테스트를 한다. ( 변경 시 오류가 없나 계속 확인 )
+ * 3. 메소드를 결정할수없는 부분을 추상클래스 -> 구현을 뒤로 미룬다. -> 익명클래스 ------ 템플릿메소드패턴
+ * 4. 특정 객체와 연결되어 있으면 끊어준다.
+ * 5. 달라지는 부분중 간단한부분은 파라미터, 복잡하다면 익명클래스를 사용해본다.
+ * 6. 템플릿 메소드의 단점 -> 변경이 생기면 추상클래스를 구현하고있던곳을 전부 바꿔야한다.
+ * 7. 단점 극복 -> 추상클래스들을 인터페이스화 시키고 매개변수로 주고 받을 수 있게 바꾼다.
+ * 8. 중복을 항상 어떻게 제거 할 수 있을까 고민해야한다.
+ */
 public class UserDao {
     public void insert(User user) {
         JdbcTemplate jdbcTemplate = new JdbcTemplate();
-        jdbcTemplate.update("INSERT INTO USERS VALUES (?, ?, ?, ?)", pstmt -> {
-            pstmt.setString(1, user.getUserId());
-            pstmt.setString(2, user.getPassword());
-            pstmt.setString(3, user.getName());
-            pstmt.setString(4, user.getEmail());
-            return pstmt;
-        });
+        jdbcTemplate.executeUpdate("INSERT INTO USERS VALUES (?, ?, ?, ?)", user.getUserId(), user.getPassword(), user.getName(), user.getEmail());
     }
 
     public void update(User user) {
         JdbcTemplate jdbcTemplate = new JdbcTemplate();
-        jdbcTemplate.update("UPDATE USERS SET password=?, name=?, email=? where userId = ?", pstmt -> {
-            pstmt.setString(1, user.getPassword());
-            pstmt.setString(2, user.getName());
-            pstmt.setString(3, user.getEmail());
-            pstmt.setString(4, user.getUserId());
-            return pstmt;
-        });
+        jdbcTemplate.executeUpdate("UPDATE USERS SET password=?, name=?, email=? where userId = ?" , user.getPassword(), user.getName(), user.getEmail(),user.getUserId());
     }
 
     public List<User> findAll() {
-        JdbcTemplate selectJdbcTemplate = new JdbcTemplate();
-        return selectJdbcTemplate.query("SELECT userId, password, name, email FROM USERS",
-                pstmt -> null, rs -> {
-            User user = null;
-            if (rs.next()) {
-                user = new User(rs.getString("userId"), rs.getString("password"), rs.getString("name"),
-                        rs.getString("email"));
-            }
-            return user;
-        });
+        JdbcTemplate jdbcTemplate = new JdbcTemplate();
+        return jdbcTemplate.executeQueryForList("SELECT userId, password, name, email FROM USERS",
+                rs -> new User(rs.getString("userId"), rs.getString("password"), rs.getString("name"),
+                            rs.getString("email")));
     }
 
     public User findByUserId(String userId) {
-        JdbcTemplate selectJdbcTemplate = new JdbcTemplate();
-        return selectJdbcTemplate.queryForObject("SELECT userId, password, name, email FROM USERS WHERE userid=?", pstmt -> {
-            pstmt.setString(1, userId);
-            return pstmt;
-        }, rs -> {
-            User user = null;
-            if (rs.next()) {
-                user = new User(rs.getString("userId"), rs.getString("password"), rs.getString("name"),
-                        rs.getString("email"));
-            }
-            return user;
-        });
+        JdbcTemplate jdbcTemplate = new JdbcTemplate();
+        return jdbcTemplate.executeQuery("SELECT userId, password, name, email FROM USERS WHERE userId=?",
+                rs -> new User(rs.getString("userId"), rs.getString("password"), rs.getString("name"), rs.getString("email")),
+                userId);
     }
 
 }
